8dc15d12a4f09f2445fe0d4b35f7276df45804b7
datnq55 <datnq.55@gmail.com>
2016-01-28 20:45:28 +0700

    - Delete JPADEMO project

--- Branches/trunk/STOCK_MANAGEMENT/src/com/luvsoft/view/dummy/StockTypeView.java
+++ Branches/trunk/STOCK_MANAGEMENT/src/com/luvsoft/view/dummy/StockTypeView.java
@@ -5,12 +5,10 @@ import java.util.List;
 import org.vaadin.viritin.fields.MTable;
 import org.vaadin.viritin.layouts.MVerticalLayout;
 
-import com.luvsoft.entities.AbstractEntity;
 import com.luvsoft.entities.Stocktype;
-import com.luvsoft.presenter.StockTypeListener;
 
 public class StockTypeView extends MVerticalLayout implements StockTypeViewInterface{
-
+    private static final long serialVersionUID = 5987085927023029095L;
     private MTable<Stocktype> table = new MTable<Stocktype>(Stocktype.class)
             .withProperties("id", "name", "description")
             .withColumnHeaders("Id", "Name", "Description")
